1317f96ef7e8a4daa305ee57a1405d2530edfd90
Jason <jasonalvarezcoraiza@gmail.com>
2019-04-03 17:47:02 -0400

    updated from repl

--- script.js
+++ script.js
@@ -2,21 +2,52 @@ var canvas = document.getElementById('gameCanvas');
 var ctx = canvas.getContext('2d');
 
 var btc = 4108.24; //USD or U.S. dollar
+var dropBTC = false;
+var btcY = 0;
+var btcGotten = 0;
 
+//power costs and positions
+
+//comp reduce points
+var powReduce = 3;
+var powReduceX = 20;
+var powReduceY = canvas.height -60;
+var powReduceHeight = 40;
+var powReduceWidth = 40;
+
+//comp reduce size
+var powSmall = 2;
+var powSmallX = 80;
+var powSmallY = canvas.height - 60;
+var powSmallHeight = 40;
+var powSmallWidth = 40;
+
+//comp reduce speed
+var powSpeed = 4;
+var powSpeedX = 140;
+var powSpeedY = canvas.height - 60;
+var powSpeedHeight = 40;
+var powSpeedWidth = 40;
+
+var overallScore = 0;
 var youScore = 0;
 var compScore = 0;
 
 var paddleHeight = 100;
 var paddleY = 450;
 
-var paddle2Y = 200;
-var paddle2Speed = 10;
-var paddle2Height = 200;
+var paddle2Y = 450;
+var paddle2Speed = 8;
+var paddle2Height = 100;
 
-var ballX = 50;
+var ballX = canvas.width/2;
 var ballSpeedX = 8;
-var ballY = 50;
+var ballY = canvas.height/2;
 var ballSpeedY = 5;
+
+var showWinScreen = false;
+
+const winningScore = 8;
 
 var framePerSecond = 50;
 
@@ -25,9 +56,14 @@ window.onload = function() {
   setInterval(function(){
 
     drawEverything();
+    console.log(btcGotten);
 
    }, 1000/framePerSecond);
 
+   canvas.addEventListener("mousedown", handleMouseClick);
+   canvas.addEventListener("mousedown", reducePoints);
+   canvas.addEventListener("mousedown", reduceSize);
+   canvas.addEventListener("mousedown", reduceSpeed);
    canvas.addEventListener("mousemove",
     function(evt){
       var mousePos = mousePosition(evt);
@@ -35,6 +71,61 @@ window.onload = function() {
     }
    )
 };
+
+function reducePoints(evt){
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top -root.scrollTop;
+  if( mouseX >= powReduceX && mouseX <= (powReduceX + powReduceWidth) ){
+    if(mouseY >= powReduceX && mouseY <= (powReduceY + powReduceHeight)) {
+      console.log("hi");
+      if(btcGotten >= powReduce){
+        btcGotten -= powReduce;
+        $("#btc").html(btcGotten + " btc");
+        compScore = Math.floor(compScore/2);
+      }
+    }
+
+  }
+}
+
+
+function reduceSize(evt){
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top -root.scrollTop;
+  if( mouseX >= powSmallX && mouseX <= (powSmallX + powSmallWidth) ){
+    if(mouseY >= powSmallX && mouseY <= (powSmallY + powSmallHeight)) {
+      console.log("hi");
+      if(btcGotten >= powSmall){
+        btcGotten -= powSmall;
+        $("#btc").html(btcGotten + " btc");
+        paddle2Height = paddle2Height/2;
+      }
+    }
+
+  }
+}
+
+function reduceSpeed(evt){
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top -root.scrollTop;
+  if( mouseX >= powSpeedX && mouseX <= (powSpeedX + powSpeedWidth) ){
+    if(mouseY >= powSpeedX && mouseY <= (powSpeedY + powSpeedHeight)) {
+      console.log("hi");
+      if(btcGotten >= powSpeed){
+        btcGotten -= powSpeed;
+        $("#btc").html(btcGotten + " btc");
+        paddle2Speed = paddle2Speed/1.5;
+      }
+    }
+
+  }
+}
 
 //gets mouse position
 
@@ -49,16 +140,93 @@ function mousePosition(evt){
   }
 }
 
+
+
 //what it sounds like (calls all other functions)
 
 function drawEverything(){
   drawRect(0, 0, canvas.width, canvas.height, "black");
+  if(showWinScreen){
+    win();
+    return;
+  }
+  if(dropBTC){
+    drawBTC();
+  }
   score(youScore, compScore);
   coins();
+  drawNet();
   drawPaddle();
+  drawReducePow();
+  drawSmallPow();
+  drawSpeedPow();
   moveBall();
   draw2Paddle();
   resetBall();
+}
+
+//draw net
+function drawNet(){
+  for(var i = 0; i<canvas.height; i+=40){
+    drawRect(canvas.width/2 -1, i, 2, 20, "white");
+  }
+}
+
+//drawing the click area for reduce compScore by floor of half
+function drawReducePow(){
+  if(powReduce <= btcGotten){
+    drawRect(powReduceX, powReduceY, powReduceWidth, powReduceHeight, "#11890d");
+    base_image.src = 'coin.png';
+    ctx.drawImage(base_image, powReduceX + 12.5, powReduceY + 10);
+  } else {
+    drawRect(powReduceX, powReduceY, powReduceWidth, powReduceHeight, "#bf251e");
+    base_image = new Image();
+    base_image.src = 'coin.png';
+    ctx.drawImage(base_image, powReduceX + 12.5, powReduceY + 10);
+  }
+}
+
+function drawSmallPow(){
+  if(powSmall <= btcGotten){
+    drawRect(powSmallX, powSmallY, powSmallWidth, powSmallHeight, "#11890d");
+    base_image.src = 'coin.png';
+    ctx.drawImage(base_image, powSmallX + 12.5, powSmallY + 10);
+  } else {
+    drawRect(powSmallX, powSmallY, powSmallWidth, powSmallHeight, "#bf251e");
+    base_image = new Image();
+    base_image.src = 'coin.png';
+    ctx.drawImage(base_image, powSmallX + 12.5, powSmallY + 10);
+  }
+}
+
+function drawSpeedPow(){
+  if(powSpeed <= btcGotten){
+    drawRect(powSpeedX, powSpeedY, powSpeedWidth, powSpeedHeight, "#11890d");
+    base_image.src = 'coin.png';
+    ctx.drawImage(base_image, powSpeedX + 12.5, powSpeedY + 10);
+  } else {
+    drawRect(powSpeedX, powSpeedY, powSpeedWidth, powSpeedHeight, "#bf251e");
+    base_image = new Image();
+    base_image.src = 'coin.png';
+    ctx.drawImage(base_image, powSpeedX + 12.5, powSpeedY + 10);
+  }
+}
+
+function win(){
+  ctx.fillStyle = "white"
+  ctx.font = "30px Trajan";
+  if(youScore >= winningScore){
+    ctx.fillText("You won!!!", canvas.width/2 -50, canvas.height/3);
+    ctx.fillText("Click To Continue", canvas.width/5 * 2, canvas.height/3 *2);
+    updateScoreBoard();
+  }
+  if(compScore >= winningScore){
+    ctx.fillText("The Computer Won", canvas.width/5 * 2, canvas.height/3);
+    ctx.fillText("Click To Continue", canvas.width/5 * 2, canvas.height/3 *2);
+  }
+
+
+
 }
 
 //ball functions
@@ -69,18 +237,26 @@ function drawBall(){
 
 function resetBall(){
   if(ballX > canvas.width){
-    ballSpeedX = -ballSpeedX;
-    ballSpeedY = -ballSpeedY;
+    var signX = Math.sign(ballSpeedX);
+    var signY = Math.sign(ballSpeedY);
+    ballSpeedX = 8 * signX;
+    ballSpeedY = 5 * signY;
     ballX = canvas.width/2;
     ballY = canvas.height/2;
+    overallScore = Math.floor(overallScore + 1);
     youScore++;
   }
   if(ballX < 0){
-    ballSpeedX = -ballSpeedX;
-    ballSpeedY = -ballSpeedY;
+    var signX = Math.sign(ballSpeedX);
+    var signY = Math.sign(ballSpeedY);
+    ballSpeedX = 8 * signX;
+    ballSpeedY = 5 * signY;
     ballX = canvas.width/2;
     ballY = canvas.height/2;
     compScore++;
+  }
+  if(youScore >= winningScore || compScore >= winningScore){
+    showWinScreen = true;
   }
 }
 
@@ -88,12 +264,14 @@ function moveBall(){
   ballX += ballSpeedX;
   ballY += ballSpeedY;
   if(paddleY < ballY && ballY < paddleHeight + paddleY && ballX <=0){
-    console.log(paddleY, ballY, paddleHeight + paddleY);
     ballSpeedX = -ballSpeedX;
+    var delta = ballY - (paddleY + paddle2Height/2);
+    ballSpeedY = delta * 0.35;
     drawCircle(ballX, ballY, 10, "white");
   }
   if(paddle2Y < ballY && ballY < (paddle2Height + paddle2Y) && ballX === canvas.width ){
-    console.log(paddle2Y, paddle2Y + paddle2Height, ballY);
+    var delta = ballY - (paddle2Y + paddle2Height/2);
+    ballSpeedY = delta * 0.35;
     ballSpeedX = -ballSpeedX;
     drawCircle(ballX, ballY, 10, "white");
   }
@@ -116,9 +294,11 @@ function draw2Paddle(){
 }
 
 function movePaddle2(){
-  paddle2Y += paddle2Speed;
-  if(paddle2Y + paddle2Height >= canvas.height || paddle2Y <= 0){
-    paddle2Speed = -paddle2Speed;
+  var paddle2Center = paddle2Y + (paddle2Height/2);
+  if(paddle2Center <= ballY && (paddle2Y + paddle2Height) <= canvas.height) {
+    paddle2Y += paddle2Speed;
+  } else {
+    paddle2Y -= paddle2Speed;
   }
 }
 
@@ -150,7 +330,7 @@ function score(score1, score2){
 
 //drop coins
 
-var coinAmount = 2;
+var coinAmount = 5;
 
 function coin(coinX, coinY, coinSpeedX, coinSpeedY, coinHeight){
   this.coinX = coinX;
@@ -163,16 +343,25 @@ function coin(coinX, coinY, coinSpeedX, coinSpeedY, coinHeight){
 var randx1Val = Math.floor(Math.random() * canvas.width);
 var coin1 = new coin(randx1Val, 0, 1, 5, 12);
 var randx2Val = Math.floor(Math.random() * canvas.width);
-var coin2 = new coin(randx2Val, 0, 2, 6, 12);
+var coin2 = new coin(randx2Val, 0, 2, 3, 12);
+var randx3Val = Math.floor(Math.random() * canvas.width);
+var coin3 = new coin(randx3Val, 0, 2, 8, 12);
+var randx4Val = Math.floor(Math.random() * canvas.width);
+var coin4 = new coin(randx4Val, 0, 2, 7, 12);
+var randx5Val = Math.floor(Math.random() * canvas.width);
+var coin5 = new coin(randx5Val, 0, 2, 6, 12);
 
 var currentCoin = [
       coin1,
-      coin2
+      coin2,
+      coin3,
+      coin4,
+      coin5
 ]
 
 function drawCoins(x){
   for(var i = 0; i<x ; i++){
-    console.log(currentCoin[i]);
+    //console.log(currentCoin[i]);
     drawCircle(currentCoin[i].coinX, currentCoin[i].coinY, currentCoin[i].coinHeight, "yellow");
     drawCircle(currentCoin[i].coinX, currentCoin[i].coinY, currentCoin[i].coinHeight/2, "orange");
   }
@@ -189,12 +378,56 @@ function coins(){
     if(currentCoin[i].coinX >= canvas.width || currentCoin[i].coinX <= 0){
       currentCoin[i].coinSpeedX = -currentCoin[i].coinSpeedX;
     }
-    if(ballX >= currentCoin[i].coinX && ballX <= (currentCoin[i].coinX + currentCoin[i].coinHeight)){
-      if(ballY >= currentCoin[i].coinY && ballY <= (currentCoin[i].coinY + currentCoin[i].coinHeight)){
+    if(ballX >= (currentCoin[i].coinX - currentCoin[i].coinHeight) && ballX <= (currentCoin[i].coinX + currentCoin[i].coinHeight)){
+      if(ballY >= (currentCoin[i].coinY - currentCoin[i].coinHeight) && ballY <= (currentCoin[i].coinY + currentCoin[i].coinHeight)){
         console.log("hit!!!");
-        youScore += btc;
+        btcGotten ++;
+        $("#btc").html(btcGotten + " btc");
+        drawBTC();
+        dropBTC = true;
+        //youScore += btc;
         currentCoin[i].coinHeight = 0;
       }
     }
   }
+}
+
+function drawBTC(){
+  ctx.fillStyle = "white"
+  ctx.font = "30px Arial";
+  if(btcGotten == 1){
+    ctx.fillText("You Got A Bit Coin!", canvas.width/2 -100, btcY);
+    ctx.fillText("They're worth " + String(btc), canvas.width/2 -100, btcY- 100);
+  } else if( btcGotten > 1){
+    ctx.fillText("You Got "+ String(btcGotten) +" Bit Coins!", canvas.width/2 -100, btcY);
+    ctx.fillText("They're worth " + String(btc), canvas.width/2 -100, btcY- 100);
+  }
+
+
+
+  btcY += 5;
+  if(btcY > canvas.width){
+    dropBTC = false;
+    btcY = 0;
+  }
+}
+
+//mouse click
+function handleMouseClick(evt){
+  if(showWinScreen){
+    youScore = 0;
+    compScore =0;
+    paddle2Height = 100;
+    paddle2Speed = 8;
+    for(var i = 0; i<coinAmount ; i++){
+    //console.log(currentCoin[i]);
+      currentCoin[i].coinHeight = 12;
+  }
+    showWinScreen = false;
+  }
+}
+
+//udate scoreboard outside of canvas
+function updateScoreBoard(){
+  $("#score").html(Math.floor(overallScore/winningScore) + " wins");
 }
